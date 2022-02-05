fbf87652c1c0b809488957ee5c91cc6d4136f6f4
asef0024 <asef0024@stud.kea.dk>
2022-02-05 21:26:46 +0100

    length and index

--- strings/strings.js
+++ strings/strings.js
@@ -1,8 +1,21 @@
 "use strict";
 
-let name = `Asefeh`;
-let animal = `kid`;
-let animalName = `Cass`;
+// let name = `Asefeh`;
+// let animal = `kid`;
+// let animalName = `Cass`;
 
-console.log(`My name is ${name}. 
-I have a ${animal} called ${animalName}.`);
+// console.log(`My name is ${name}. 
+// I have a ${animal} called ${animalName}.`);
+
+let name = "Albus Percival Wulfric Brian Dumbledore";
+let firstName = "Dumbledore"
+let total = name.length;
+console.log(total);
+let indexTwo = name[2];
+console.log(indexTwo);
+let indexSix = name[6];
+console.log(indexSix);
+let firstD = "D";
+console.log(firstName.indexOf(firstD));
+let lastE = "e";
+console.log(firstName.lastIndexOf(lastE));
