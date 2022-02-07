cca1e3fb891b9fb8986a0e9db2b131841bdaf9ee
asef0024 <asef0024@stud.kea.dk>
2022-02-07 20:30:58 +0100

    pitalization

---
+++ 3caPitalization/3caPi.js
@@ -0,0 +1,7 @@
+"use strict"
+
+const myName= "peter";
+
+const peTer= console.log(myName.substring(0,2)+myName.substring(2,3).toUpperCase()+myName.substring(3));
+
+const PeteR = console.log(myName.charAt(0).toUpperCase()+myName.substring(1,4)+myName.substring(4).toUpperCase());
