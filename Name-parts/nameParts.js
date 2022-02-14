2c6e56630c09b92b61e21030c57c3440ad6a4101
asef0024 <asef0024@stud.kea.dk>
2022-02-14 15:22:08 +0100

    nameParts w 3 parameters

--- Name-parts/nameParts.js
+++ Name-parts/nameParts.js
@@ -6,11 +6,27 @@ const firstName = console.log(myName.substring(myName.indexOf(0), myName.indexOf
 const middleName = console.log(myName.substring(myName.indexOf(" "), myName.lastIndexOf(" ")).trim());
 const lastName = console.log(myName.substring(myName.lastIndexOf(" ")).trim());
 
+
+//nameParts functions w parameters
 function getNameParts (fullname) {
     const firstName = fullname.substring(fullname.indexOf(0), fullname.indexOf(" "));
     const middleName = fullname.substring(fullname.indexOf(" "), fullname.lastIndexOf(" ")).trim();
     const lastName = fullname.substring(fullname.lastIndexOf(" ")).trim();
     console.log( { firstName, middleName, lastName } );
+    // fullname("Potter Harry James");
 }
 
 getNameParts("Asefeh Luca Zivlaei");
+
+function fullname(lastName, firstName, middleName) {
+    if (middleName == undefined) {
+        console.log(firstName + " " + lastName)
+    } else {
+    console.log(firstName + " " + middleName + " " + lastName); 
+}
+}
+
+fullname("Potter", "Harry", "James");
+fullname("Harry" , "Potter");
+fullname("Potter", "Harry");
+fullname("Potter", "Harry", "James", "Pottypotpot");
