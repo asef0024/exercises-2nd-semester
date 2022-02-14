10915e16193f2d99ec10a219a3b70156928ec9c5
asef0024 <asef0024@stud.kea.dk>
2022-02-14 14:31:06 +0100

    func w param

---
+++ Func w parameter/js.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let aNumber = 3;
+function addingNumber(aNumber, bNumber){
+    console.log(aNumber + bNumber)
+}
+
+addingNumber(2,4);
+
+
+
+sayHello("Asefeh");
+
+function sayHello (firstName){
+   
+    console.log(`Hello ${firstName}`);
+};
+
+function presentPet(firstName, animalType, animalName) {
+    console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName}`);
+}
+
+presentPet("Asefeh", "dog", "Vovov")
