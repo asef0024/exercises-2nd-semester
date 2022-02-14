ec89020b600ba004dd95056215daeb7a966fd99b
asef0024 <asef0024@stud.kea.dk>
2022-02-14 14:55:06 +0100

    full name w function

--- Name-parts/nameParts.js
+++ Name-parts/nameParts.js
@@ -5,3 +5,12 @@ const myName = "Peter Heronimous Lind"
 const firstName = console.log(myName.substring(myName.indexOf(0), myName.indexOf(" ")));
 const middleName = console.log(myName.substring(myName.indexOf(" "), myName.lastIndexOf(" ")).trim());
 const lastName = console.log(myName.substring(myName.lastIndexOf(" ")).trim());
+
+function getNameParts (fullname) {
+    const firstName = fullname.substring(fullname.indexOf(0), fullname.indexOf(" "));
+    const middleName = fullname.substring(fullname.indexOf(" "), fullname.lastIndexOf(" ")).trim();
+    const lastName = fullname.substring(fullname.lastIndexOf(" ")).trim();
+    console.log( { firstName, middleName, lastName } );
+}
+
+getNameParts("Asefeh Luca Zivlaei");
